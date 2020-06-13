a3076cad91537f1079d999194a78b44dcf6f0d2b
Kevin Laeufer <kevin.laeufer@sifive.com>
2020-06-12 17:24:17 -0700

    add Serializer test

--- src/test/scala/firrtlTests/ir/SerializeSpec.scala
+++ src/test/scala/firrtlTests/ir/SerializeSpec.scala
@@ -11,7 +11,25 @@ class SerializeSpec extends FlatSpec {
     assert(FileInfo(StringLit("test.scala:80")).serialize == " @[test.scala:80]")
     assert(FileInfo(StringLit("test.scala:\n80")).serialize == " @[test.scala:\\n80]")
     assert(FileInfo(StringLit("test.scala:]80")).serialize == " @[test.scala:\\]80]")
+  }
 
+  "Serializer" should "serialize a module correctly" in {
+    val in =
+      """circuit m :
+        |  module m :
+        |    input clk : Clock
+        |    input reset : UInt<1>
+        |    output x : UInt<1>
+        |    clk is invalid
+        |    x is invalid
+        |""".stripMargin.trim
 
+    val c = firrtl.Parser.parse(in)
+    val out = Serializer.serialize(c)
+    val outOld = c.serialize
+    in.split('\n').zip(out.split('\n')).zip(outOld.split('\n')).zipWithIndex.foreach { case (((il, ol), ool), line) =>
+      assert(il == ol, s"failed in line $line")
+      assert(ool == ol, s"failed in line $line")
+    }
   }
 }
