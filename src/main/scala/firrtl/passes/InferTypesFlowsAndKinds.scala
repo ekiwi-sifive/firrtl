2cb77a59dbcdc1a2321425047ff6033df2f9828b
Kevin Laeufer <kevin.laeufer@sifive.com>
2020-07-07 14:21:57 -0700

    InferTypesFlowsAndKinds: implement resolve flow idea

--- src/main/scala/firrtl/passes/InferTypesFlowsAndKinds.scala
+++ src/main/scala/firrtl/passes/InferTypesFlowsAndKinds.scala
@@ -32,8 +32,8 @@ object InferTypesFlowsAndKinds extends Pass {
   }
 
   private def onModule(replaceUnknowns: ReplaceUnknowns, moduleTypes: Map[String, Type])(m: DefModule): DefModule = {
-    val lut = new Lookup(replaceUnknowns, moduleTypes)
-    m.map(onPort(_)(lut)).map(onStmt(_)(lut))
+    implicit val lut: Lookup = new Lookup(replaceUnknowns, moduleTypes)
+    m.map(onPort).map(onStmt)
   }
 
   private def onPort(p: Port)(implicit lut: Lookup): Port = {
@@ -69,26 +69,40 @@ object InferTypesFlowsAndKinds extends Pass {
 
   //scalastyle:off cyclomatic.complexity
   /** Replace all expressions with their typed, known-flow and known-kind versions.
-    * @note Type propagates bottom-up, a->b->c for `a.b.c`.
+    * @note Type propagates bottom-up / left-to-right, e.g., a->b->c for `a.b.c`.
     *       This is why the `lut` needs to operate on already visited expressions.
-    * @note Flow propagates top-down, c->b->a for `a.b.c` (which will be represented as `(((a) b) c)`).
+    * @note Flow propagates top-down / right-to-left, e.g., c->b->a for `a.b.c`
+    *       (which will be represented as `(((a) b) c)`).
     *       Flow also relies on Type in that it needs to know the Field type in order to determine
     *       whether a field is flipped.
     *       In order to reconcile the opposite propagation and the co-dependence, we remember
-    *       all field names that were used to access a Reference (`fieldTrace`) which allows us to resolve
-    *       the Flow at the leaf node and then propagate it bottom up,
+    *       all field names that were used to access a Reference (`fieldTrace`) which allows us to
+    *       resolve the Flow at the leaf node.
     * @note Kind only matters at the Reference leaf nodes.
     */
-  private def onExpr(f: Flow, fieldTrace: List[String])(expr: Expression)(implicit lut: Lookup): Expression = {
+  private def onExpr(f: Flow, fieldTrace: List[String] = List())(expr: Expression)(implicit lut: Lookup): Expression = {
     val subTrace = expr match {
-      case SubField(_, name, _, _) => fieldTrace :+ name
+      case SubField(_, name, _, _) => name +: fieldTrace
       case _ => fieldTrace
     }
     expr.map(onExpr(f, subTrace)) match {
-      case e: Reference => lut.reference(e.name, f)
-      case e: SubField => lut.subField(e.expr, e.name, f)
-      case e: SubIndex => lut.subIndex(e.expr, e.value, f)
-      case e: SubAccess => lut.subAccess(e.expr, e.index, f)
+      case e: Reference =>
+        val base = lut.reference(e.name, f)
+        if(fieldTrace.nonEmpty && isFlipped(base.tpe, fieldTrace)) {
+          lut.reference(e.name, flip(f))
+        } else { base }
+      case e: SubField =>
+        val fieldType = Utils.field_type(e.expr.tpe, e.name)
+        val flow = if(fieldTrace.nonEmpty && isFlipped(fieldType, fieldTrace)) { flip(f) } else { f }
+        lut.subField(e.expr, e.name, flow)
+      case e: SubIndex =>
+        val dataType = Utils.sub_type(e.expr.tpe)
+        val flow = if(fieldTrace.nonEmpty && isFlipped(dataType, fieldTrace)) { flip(f) } else { f }
+        lut.subIndex(e.expr, e.value, flow)
+      case e: SubAccess =>
+        val dataType = Utils.sub_type(e.expr.tpe)
+        val flow = if(fieldTrace.nonEmpty && isFlipped(dataType, fieldTrace)) { flip(f) } else { f }
+        lut.subAccess(e.expr, e.index, flow)
       // type inference for non-reference expressions
       case e: DoPrim => e.copy(tpe = e.op.propagateType(e))
       case e: Mux => e.copy(tpe = Utils.mux_type_and_widths(e.tval, e.fval))
@@ -98,6 +112,19 @@ object InferTypesFlowsAndKinds extends Pass {
     }
   }
   //scalastyle:on cyclomatic.complexity
+
+  private def flip(o: Flow): Flow = o match {
+    case SourceFlow => SinkFlow
+    case SinkFlow => SourceFlow
+  }
+
+  private def isFlipped(tpe: Type, fieldTrace: List[String]): Boolean = tpe match {
+    case BundleType(fields) =>
+      val field = fields.find(_.name == fieldTrace.head).get
+      isFlipped(field.tpe, fieldTrace.tail) ^ (field.flip == Flip)
+    case VectorType(tpe, _) => isFlipped(tpe, fieldTrace)
+    case _ => assert(fieldTrace.isEmpty) ; false
+  }
 }
 
 /** Keeps track of all reference and accessor types, kinds and flows inside one module.
@@ -110,35 +137,33 @@ private class Lookup(replace: ReplaceUnknowns, val moduleTypes: Map[String, Type
   def declare(name: String, tpe: Type, kind: Kind): Type = {
     val t = replaceUnknowns(tpe)
     // We eagerly generate all possible references for both, the lvalue (SinkFlow) and rvalue (SourceFlow) cases.
-    maps(SourceFlow).refs(name) = Reference(name, t, kind, SourceFlow)
-    maps(SinkFlow).refs(name) = Reference(name, t, kind, SinkFlow)
+    refs(SourceFlow)(name) = Reference(name, t, kind, SourceFlow)
+    refs(SinkFlow)(name) = Reference(name, t, kind, SinkFlow)
     t
   }
 
   // Access the unique accessor expressions.
   // Note that sub accesses will only work if you first resolve the expressions in them.
-  def reference(name: String, flow: Flow): Reference = maps(flow).refs(name)
+  def reference(name: String, flow: Flow): Reference = refs(flow)(name)
   def subField(expr: Expression, name: String, flow: Flow): SubField =
-    maps(flow).field.getOrElseUpdate(IdAndEqKey(expr, name),
+    fields.getOrElseUpdate(IdAndEqKey(expr, (flow, name)),
       SubField(expr, name, Utils.field_type(expr.tpe, name), flow))
   def subIndex(expr: Expression, index: Int, flow: Flow): SubIndex =
-    maps(flow).index.getOrElseUpdate(IdAndEqKey(expr, index),
+    indices.getOrElseUpdate(IdAndEqKey(expr, (flow, index)),
       SubIndex(expr, index, Utils.sub_type(expr.tpe), flow))
   def subAccess(expr: Expression, index: Expression, flow: Flow): SubAccess =
-    maps(flow).access.getOrElseUpdate(IdSeqKey(Seq(expr, index)),
+    accesses.getOrElseUpdate(IdSeqAndEqKey(Seq(expr, index), flow),
       SubAccess(expr, index, Utils.sub_type(expr.tpe), flow))
 
   // Expose the replace type functionality in order to handle the DefMemory special case.
   def replaceUnknowns(tpe: Type): Type = replace(tpe)
 
   // Remember expressions in order to ensure that objects are interned as much as possible.
-  private class Maps {
-    val refs   = mutable.HashMap[String, Reference]()
-    val field  = mutable.HashMap[IdAndEqKey[Expression, String], SubField]()
-    val index  = mutable.HashMap[IdAndEqKey[Expression, Int], SubIndex]()
-    val access = mutable.HashMap[IdSeqKey[Expression], SubAccess]()
-  }
-  private val maps: Map[Flow, Maps] = Seq(SourceFlow -> new Maps(), SinkFlow -> new Maps()).toMap
+  private val fields = mutable.HashMap[IdAndEqKey[Expression, (Flow, String)], SubField]()
+  private val indices = mutable.HashMap[IdAndEqKey[Expression, (Flow, Int)], SubIndex]()
+  private val accesses = mutable.HashMap[IdSeqAndEqKey[Expression, Flow], SubAccess]()
+  private type RefMap = mutable.HashMap[String, Reference]
+  private val refs: Map[Flow, RefMap] = Seq(SourceFlow -> new RefMap(), SinkFlow -> new RefMap()).toMap
 }
 
 /** Substitutes Unknown Width and Bound with unique variables. */
