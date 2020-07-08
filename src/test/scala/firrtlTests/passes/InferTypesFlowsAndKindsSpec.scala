23c118ffcf74af8f1abad4608a99880c35e2268d
Kevin Laeufer <kevin.laeufer@sifive.com>
2020-07-08 11:19:42 -0700

    test unknown width inference of module ports

--- src/test/scala/firrtlTests/passes/InferTypesFlowsAndKindsSpec.scala
+++ src/test/scala/firrtlTests/passes/InferTypesFlowsAndKindsSpec.scala
@@ -2,9 +2,10 @@
 
 package firrtlTests.passes
 
+import firrtl.ir.SubField
 import firrtl.options.Dependency
 import firrtl.stage.TransformManager
-import firrtl.{CircuitState, InstanceKind, NodeKind, PortKind, RegKind, SourceFlow, WireKind, ir, passes}
+import firrtl.{CircuitState, InstanceKind, NodeKind, PortKind, RegKind, SinkFlow, SourceFlow, WireKind, ir, passes}
 import org.scalatest._
 
 class InferTypesFlowsAndKindsSpec extends FlatSpec {
@@ -27,6 +28,8 @@ class InferTypesFlowsAndKindsSpec extends FlatSpec {
     case ir.Conditionally(_, _, a, b) => Seq(a,b).flatMap(getConnects)
     case _ => Seq()
   }
+  private def getModule(c: ir.Circuit, name: String): ir.Module =
+    c.modules.find(_.name == name).get.asInstanceOf[ir.Module]
 
   it should "infer references to ports, wires, nodes and registers" in {
     val node = getNodes(infer(
@@ -69,7 +72,7 @@ class InferTypesFlowsAndKindsSpec extends FlatSpec {
   }
 
   it should "infer types for references to instances" in {
-    val m = infer(
+    val m = getModule(infer(
       """circuit m:
         |  module other:
         |    output x: { y: UInt, flip z: UInt<1> }
@@ -77,35 +80,86 @@ class InferTypesFlowsAndKindsSpec extends FlatSpec {
         |    inst i of other
         |    node i_x = i.x
         |    node i_x_y = i.x.y
+        |    node i_x_y_2 = i_x.y
         |    node a = UInt<1>(1)
         |    i.x.z <= a
-        |""".stripMargin).modules.head.asInstanceOf[ir.Module]
+        |""".stripMargin), "m")
     val node = getNodes(m.body).toMap
     val con = getConnects(m.body)
 
-    // TODO: update below
 
-    // node x_y = x.y
-    assert(node("x_y").tpe.isInstanceOf[ir.UIntType])
+    // node i_x_y = i.x.y
+    assert(node("i_x_y").tpe.isInstanceOf[ir.UIntType])
     // the type inference replaces all unknown widths with a variable
-    assert(node("x_y").tpe.asInstanceOf[ir.UIntType].width.isInstanceOf[ir.VarWidth])
-    assert(node("x_y").asInstanceOf[ir.SubField].flow == SourceFlow)
-    val x = node("x_y").asInstanceOf[ir.SubField].expr.asInstanceOf[ir.Reference]
+    assert(node("i_x_y").tpe.asInstanceOf[ir.UIntType].width.isInstanceOf[ir.VarWidth])
+    assert(node("i_x_y").asInstanceOf[ir.SubField].flow == SourceFlow)
+
+
+    // node i_x = i.x
+    val x = node("i_x_").asInstanceOf[ir.SubField]
     assert(x.tpe.isInstanceOf[ir.BundleType])
     assert(x.tpe.asInstanceOf[ir.BundleType].fields.head.name == "y")
-    assert(x.tpe.asInstanceOf[ir.BundleType].fields.head.tpe == node("x_y").tpe)
+    assert(x.tpe.asInstanceOf[ir.BundleType].fields.head.tpe == node("i_x_y").tpe)
     assert(x.tpe.asInstanceOf[ir.BundleType].fields.head.flip == ir.Default)
     assert(x.tpe.asInstanceOf[ir.BundleType].fields.last.flip == ir.Flip)
-    assert(x.kind == InstanceKind)
     assert(x.flow == SourceFlow)
-    val portXType = x.tpe
 
-    // x.z <= a
+    val i = x.expr.asInstanceOf[ir.Reference]
+    assert(i.kind == InstanceKind)
+    assert(i.flow == SourceFlow)
+
+
+    // node i_x_y_2 = i_x.y
+    assert(node("i_x_y").tpe == node("i_x_y_2").tpe)
+    assert(node("i_x_y").asInstanceOf[ir.SubField].flow == node("i_x_y_2").asInstanceOf[ir.SubField].flow)
+
+
+    // i.x.z <= a
     val (left, right) = (con.head.loc.asInstanceOf[ir.SubField], con.head.expr.asInstanceOf[ir.Reference])
-    //assert(left == ir.SubField(ir.Reference()))
+
+    // flow propagates z -> x -> i
+    assert(left.flow == SinkFlow)
+    val left_x = left.expr.asInstanceOf[SubField]
+    assert(left_x.flow == SourceFlow) // flip z
+    val left_i = left_x.expr.asInstanceOf[ir.Reference]
+    assert(left_i.flow == SourceFlow)
+
+    assert(left_i.kind == InstanceKind)
+    assert(left_x.tpe == x.tpe)
+  }
+
+  it should "infer types for references to memories" in {
+
+
 
   }
 
-  it should "infer types for references to memories" in {}
+  it should "infer different instances of the same module to have the same width variable" in {
+    val c = infer(
+      """circuit m:
+        |  module other:
+        |    input x: UInt
+        |  module x:
+        |    inst i of other
+        |    i.x <= UInt<16>(3)
+        |  module m:
+        |    inst x of x
+        |    inst i of other
+        |    i.x <= UInt<1>(1)
+        |""".stripMargin)
+    val m_con = getConnects(getModule(c, "m").body).head
+    val x_con = getConnects(getModule(c, "x").body).head
+    val other = getModule(c, "other")
+
+    // this is the type of the other.x port
+    val tpe = m_con.loc.tpe.asInstanceOf[ir.UIntType]
+    assert(tpe.width.isInstanceOf[ir.VarWidth])
+    // since it is the only unknown width, it should just be replaced with a "w"
+    assert(tpe.width.asInstanceOf[ir.VarWidth].name == "w")
+
+    assert(m_con.loc.tpe == tpe)
+    assert(x_con.loc.tpe == tpe)
+    assert(other.ports.head.tpe == tpe)
+  }
 
 }
